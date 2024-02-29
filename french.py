ce36534fbfe04f06e340468e9ea8daf14c518330
Tom Cogzell <58867012+C0gzy@users.noreply.github.com>
2024-02-29 18:16:25 +0000

    Further Improvements

--- french.py
+++ french.py
@@ -7,8 +7,6 @@ import json
 with open("../FrenchVerbRevision/Questions.json") as file:
     Database = json.load(file)
 
-print(r.choice(list(Database.keys())))
-
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack
@@ -19,10 +17,7 @@ windowHeight = root.winfo_reqheight()
 positionRight = int(root.winfo_screenwidth()/2 - windowWidth/2)
 positionDown = int(root.winfo_screenheight()/2 - windowHeight/2)
 
-a_text = ""
-b_text = ""
-c_text = ""
-
+words = []
 
 def SelectWord():
     CurrentWord = r.choice(list(Database.keys())) 
@@ -35,26 +30,10 @@ def SelectWord():
 SelectWord.WordAlreadySelected = []
 
 for picked in range(3):
-    if picked == 0:
-        a_text = SelectWord() 
-    elif picked == 1:
-        b_text = SelectWord()
-    elif picked == 2:
-        c_text = SelectWord()
- 
-picked_ans = 0
-find = ""
-ans = r.randint(1,3)
-if ans == 1:
-    find = find = a_text
-elif ans == 2:
-    find = find = b_text
-elif ans == 3:
-    find = find = c_text
+    words.append(SelectWord())
 
-HEIGHT = 500
-WIDTH = 600
-
+ans = r.randint(0,2)
+find = words[ans]
 
 def check_correct(char):
     if char == ans:
@@ -65,9 +44,9 @@ def check_correct(char):
     os.execl(os.sys.executable,*([os.sys.executable]+os.sys.argv))
 
 #                            |word          |command
-A = tk.Button(root, text =a_text, command = lambda :check_correct(1))
-B = tk.Button(root, text =b_text, command = lambda :check_correct(2))
-C = tk.Button(root, text =c_text, command = lambda :check_correct(3))
+A = tk.Button(root, text =words[0], command = lambda :check_correct(0))
+B = tk.Button(root, text =words[1], command = lambda :check_correct(1))
+C = tk.Button(root, text =words[2], command = lambda :check_correct(2))
 
 
 QuestionAnswer = Database[find]["Questions"]
